0e65a481dc25d27545ab30c722b7529a5e554db6
Abhimanyu Shahi <abhimanyushahi88@gmail.com>
2024-02-21 02:07:06 +0530

    space separated array

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Accept command-line arguments
-instance_array=("$@")
-parameter1=$1
-parameter2=$2
+instance_array=($1)
+parameter1=$2
+parameter2=$3
 
 # Print the array items, parameter1, and parameter2
 echo "Instance array:"
